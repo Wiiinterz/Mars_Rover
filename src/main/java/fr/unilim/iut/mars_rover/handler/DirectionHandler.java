f623ed985826a331318c9c4045adeec400e29994
Hashual <loizeau.dorian05@gmail.com>
2024-11-19 15:29:33 +0100

    Start : add unfinished handler

--- src/main/java/fr/unilim/iut/mars_rover/handler/DirectionHandler.java
+++ src/main/java/fr/unilim/iut/mars_rover/handler/DirectionHandler.java
@@ -1,5 +1,15 @@
 package fr.unilim.iut.mars_rover.handler;
 
-public class DirectionHandler {
+import fr.unilim.iut.mars_rover.Rover;
 
-}
+public abstract class DirectionHandler {
+	private DirectionHandler nextHandler;
+	private String command;
+	
+	public DirectionHandler(DirectionHandler nextHandler) {
+		this.nextHandler = nextHandler;
+	}
+	
+	public void handle(command);
+		
+	}
--- src/main/java/fr/unilim/iut/mars_rover/handler/ForwardHandler.java
+++ src/main/java/fr/unilim/iut/mars_rover/handler/ForwardHandler.java
@@ -1,5 +1,24 @@
 package fr.unilim.iut.mars_rover.handler;
 
-public class ForwardHandler {
+import fr.unilim.iut.mars_rover.Move;
+import fr.unilim.iut.mars_rover.Rover;
+
+public class ForwardHandler extends DirectionHandler {
+	
+	public ForwardHandler(DirectionHandler nextHandler) {
+		super(nextHandler);
+	}
+	
+	@Override
+	public void handle(String command) {
+		char letter = command.charAt(0);
+		Rover rover;
+		Move move;
+		
+		if (letter='f') {
+			rover.setState(N);
+			move.moveInADirection(rover);
+		}
+	}
 
 }
